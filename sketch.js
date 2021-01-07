76a981cc015dd275713e401483b8c2a5395725d8
bruwbird <stm1051212@yahoo.co.jp>
2021-01-07 15:18:52 +0900

    configuration

--- sketch.js
+++ sketch.js
@@ -5,36 +5,40 @@ function setup() {
     createCanvas(windowWidth, windowHeight);
     userStartAudio();
 
-    angleMode(DEGREES); // Change the mode to DEGREES
-
     mic = new p5.AudioIn();
     mic.start();
 }
 
 function draw() {
-    translate(width / 2, height / 2)
+
     background(0);
     fill(255);
     micLevel = mic.getLevel();
 
-    var sd = micLevel * 100;
-    circle(0, 0, sd)
-
     // see https://medium.com/@nishancw/audio-visualization-in-javascript-with-p5-js-cf3bc7f1be07
     noFill();
     beginShape();
-    volHistory.push(micLevel);
+    volHistory.push(micLevel * 3);
     for (let i = 0; i < 360; i++) {
         stroke(255);
         let r = map(volHistory[i], 0, 1, 10, 300);
         let x = r * cos(i);
         let y = r * sin(i);
-        vertex(x, y);
+        vertex(x + windowWidth / 2, y + windowHeight / 2);
     }
     endShape();
 
     if (volHistory.length > 360) {
         volHistory.splice(0, 1);
+    }
+
+    for (i = 0; i < micLevel * 3000; i++) {
+        var x = new Array(4);
+        for (j = 0; j < 4; j++) {
+            x[j] = random();
+        }
+        stroke(color(x[0] * 255, x[1] * 255, x[2] * 255, 30));
+        bezier(windowWidth / 2, windowHeight / 2, x[0] * windowWidth, x[1] * windowHeight, x[2] * windowWidth, x[3] * windowHeight, windowWidth / 2, windowHeight / 2);
     }
 
 }
